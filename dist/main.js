fb47e3ef801120cd032e184e9dfa1a0cf24aba56
Wiebke Bienk <wiebke.bienk@gmail.com>
2022-07-12 13:52:43 +0200

    Aufgabe a erledigt

---
+++ dist/main.js
@@ -0,0 +1,15 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const button = document.getElementById("Check")
+
+button.addEventListener("click", checkISBN)
+
+function checkISBN() {
+    alert("checkISBN");
+}
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
